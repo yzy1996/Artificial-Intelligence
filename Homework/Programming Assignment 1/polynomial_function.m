b38d6405c9829bc425a579c6288931560d3fd2ab
crazyang <yang3781590@163.com>
2019-10-15 16:10:03 +0800

    Update polynomial_function.m

--- Homework/Programming Assignment 1/polynomial_function.m
+++ Homework/Programming Assignment 1/polynomial_function.m
@@ -12,25 +12,19 @@ polyy = poly_data.polyy;
 
 m = length(Sampx);
 
+f3 = figure;
+error = zeros(5,5);
+
 %% control training data
 % random subset of samples 10%, 25%, 50%, 75%
 
-error = zeros(5,5);
 K = 5;
 percent = [1, 0.75, 0.5, 0.25 ,0.1];
-iteration = 100;
-subtitle = ["75% data", "50% data", "25% data", "10% data"]
+iteration = 20;
+subtitle = ["75% data", "50% data", "25% data", "10% data"];
+
 
 for subset = 1:5
-	
-	if subset == 1
-		figure
-		
-		figure
-		plot(polyx,sqrt(diag(sigmaa)))
-		title('standard deviation around the mean')
-		contine
-	end
 	
     for iter = 1:iteration
         
@@ -53,48 +47,93 @@ for subset = 1:5
         [theta5,sigmaa] = mybr;
         error(subset, 5) = error(subset, 5) + mymse(theta5);
         
+        %%%%% Problem2
+        if subset == 1
+            figure
+            myplot
+            title('plot of the estimated function')
+            
+            figure
+            plot(polyx,sqrt(diag(sigmaa)),'LineWidth',2)
+            title('standard deviation around the mean')
+            break
+        end
+        
     end
 	
-    %% show the data plot
+    if subset == 1
+        continue
+    end
+    
+    %%%%% Problem3
+
+    figure(f3);
     subplot(2, 2, subset-1)
 	myplot
+    title(subtitle(subset-1))
    
+    %% Add outliers output values
+    % add large numbers to a few values in sampy
+    
+    if subset == 4
+        err = zeros(1,5);
+        Sampy(suborder) = Sampy(suborder) + 50;
+        sampx = Sampx;
+        sampy = Sampy;
+        Phi = mytrans(sampx);
+        theta1 = myls;
+        err(1) = mymse(theta1);
+        theta2 = myrls;
+        err(2) = mymse(theta2);
+        theta3 = mylasso;
+        err(3) = mymse(theta3);
+        theta4 = myrr;
+        err(4) = mymse(theta4);
+        [theta5,~] = mybr;
+        err(5) = mymse(theta5);
+        figure
+        myplot
+        title('outliers output values')
+    end
 end
 
 error = sqrt(error);
+error(2:5,1:5) = error(2:5,1:5)/iteration;
+myerrorplot
+
+% print MSE
+% method = ["least-squares", "regularized LS", "L1-regularized LS", "robust regression", "Bayesian regression"];
+% fprintf('the %s algorithm average mean-squared error is %f\n', [method;error(1,:)])
+% fprintf('the %s algorithm average mean-squared error is %f\n', [method;error(2,:)])
+% fprintf('the %s algorithm average mean-squared error is %f\n', [method;error(3,:)])
+% fprintf('the outliers %s algorithm average mean-squared error is %f\n', [method;err])
+
 
 % plot error responding reduced data
 % ignore 10% because of too high error value
-% 我的目的是看到 1、在同一个算法下，随着样本的减少，误差是越来越大的
-% 2、不同算法之间看出差异
-figure
-hold on
-plot(percent(1:4),error(1:4,1)/iteration)
-plot(percent(1:4),error(1:4,2)/iteration)
-plot(percent(1:4),error(1:4,3)/iteration)
-plot(percent(1:4),error(1:4,4)/iteration)
-plot(percent(1:4),error(1:4,5)/iteration)
-set(gca,'YDir','reverse')%对Y方向反转
-set(gca,'YDir','normal')
-legend("least-squares", "regularized LS", "L1-regularized LS", "robust regression", "Bayesian regression")
-hold off
-
-% 输出MSE
-method = ["least-squares", "regularized LS", "L1-regularized LS", "robust regression", "Bayesian regression"];
-fprintf('the %s algorithm average mean-squared error is %f\n', [method;error(4,:)/iteration])
-
+    function myerrorplot
+        figure
+        hold on
+        plot(percent(1:4),error(1:4,1),'LineWidth',1)
+        plot(percent(1:4),error(1:4,2),'LineWidth',1)
+        plot(percent(1:4),error(1:4,3),'--','LineWidth',1)
+        plot(percent(1:4),error(1:4,4),'LineWidth',1)
+        plot(percent(1:4),error(1:4,5),'LineWidth',1)
+        set(gca,'XDir','reverse')
+        legend("least-squares", "regularized LS", "L1-regularized LS", "robust regression", "Bayesian regression")
+        hold off
+    end
 
 	function myplot
 		hold on
-		scatter(sampx,sampy,'.')
+		scatter(sampx,sampy, 100, '.')
 		plot(polyx, polyy, 'red')
-		plot(polyx, polyval(flipud(theta1),polyx), 'green*')
-		plot(polyx, polyval(flipud(theta2),polyx), 'cyano')
-		plot(polyx, polyval(flipud(theta3),polyx), 'blue')
-		plot(polyx, polyval(flipud(theta4),polyx), 'black')
-		plot(polyx, polyval(flipud(theta5),polyx), 'magenta')
+		plot(polyx, polyval(flipud(theta1),polyx), 'green', 'LineWidth',1)
+		plot(polyx, polyval(flipud(theta2),polyx), 'cyan', 'LineWidth',1)
+		plot(polyx, polyval(flipud(theta3),polyx), 'blue--', 'LineWidth',1)
+		plot(polyx, polyval(flipud(theta4),polyx), 'black', 'LineWidth',1)
+		plot(polyx, polyval(flipud(theta5),polyx), 'magenta', 'LineWidth',1)
 		
-		title(subtitle(subset-1))
 		xlabel('x')
 		ylabel('y')
 		legend('samples','function','least-squares','regularized LS','L1-regularized','robust regression','Bayesian regression')
@@ -123,13 +162,13 @@ fprintf('the %s algorithm average mean-squared error is %f\n', [method;error(4,:
         H = [Phi*Phi' -Phi*Phi';-Phi*Phi' Phi*Phi'];
         f = lambda*ones(2*(K+1),1)-[Phi*sampy;-Phi*sampy];
         x = quadprog(H,f);
-        theta3 = x(1:K+1) - x(K+2:K+7);
+        theta3 = x(1:K+1) - x(K+2:2*K+2);
         
     end
 
 %% Robust regression
     function theta4 = myrr
-        
+        n = length(sampx);
         f = [zeros(1,K+1) ones(1,n)]';
         A = [-Phi' -eye(n);Phi' -eye(n)];
         b = [-sampy sampy]';
@@ -149,10 +188,9 @@ fprintf('the %s algorithm average mean-squared error is %f\n', [method;error(4,:
         
     end
 
-%% function
+%% other function
     function err = mymse(theta)
-        % mean-squared error
-        
+        % mean-squared error        
         err = sum((polyy'-polyval(flipud(theta),polyx)).^2);
     end
 

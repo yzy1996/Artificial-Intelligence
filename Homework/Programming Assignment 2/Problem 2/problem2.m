0249379fa8985a661c8a551223a34123e25b1139
crazyang <yang3781590@163.com>
2019-11-06 21:47:03 +0800

    Create problem2.m

---
+++ Homework/Programming Assignment 2/Problem 2/problem2.m
@@ -0,0 +1,36 @@
+clc;clear;
+% read the image and view it
+img = imread('images/12003.jpg');
+subplot(1,3,1); imagesc(img); axis image;
+
+% extract features (stepsize = 7)
+[X, L] = getfeatures(img, 7);
+XX = [X(1:2,:) ; X(3:4,:)/10]; % downscale the coordinate features (see part (b))
+
+% run kmeans -- this is the MATLAB version. You have to write your own!
+% [Y, C] = kmeans(XX, 2);
+Y = meanshift(XX);
+
+% make a segmentation image from the labels
+segm = labels2segm(Y, L);
+subplot(1,3,2); imagesc(segm); axis image;
+
+% color the segmentation image
+csegm = colorsegm(segm, img);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+subplot(1,3,3); imagesc(csegm); axis image

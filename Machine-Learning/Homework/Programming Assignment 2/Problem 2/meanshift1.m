2b16b432f6ba3fae7488afae5eae40d985d166ac
crazyang <yang3781590@163.com>
2019-12-02 17:48:30 +0800

    Create meanshift1.m

---
+++ Machine-Learning/Homework/Programming Assignment 2/Problem 2/meanshift1.m
@@ -0,0 +1,27 @@
+function Y = meanshift1(x)
+
+iteration = 300;
+[d, n] = size(x);
+h = 1;
+xx = x;
+
+for i = 1:n
+
+    look = xx(:,i);
+    
+    for iter = 1:iteration
+        xx(:,i) = x * mvnpdf(x', xx(:,i)', h^2 * eye(d)) / sum(mvnpdf(x', xx(:,i)', h^2 * eye(d)));
+        
+        if round(look, 10) == round(xx(:,i), 10)
+            break
+        end
+        look = xx(:,i);
+    end
+end
+
+Y = round(xx(1, :));
+
+% Y(Y < ((max(Y)+min(Y))/2)) = 1;
+% Y(Y >= ((max(Y)+min(Y))/2)) = 2;
+
+end

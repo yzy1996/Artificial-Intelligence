f6f041fb41c695d168e19cada65177eac2f1dad5
crazyang <yang3781590@163.com>
2019-12-02 17:48:25 +0800

    Update emgmm.m

--- Machine-Learning/Homework/Programming Assignment 2/Problem 2/emgmm.m
+++ Machine-Learning/Homework/Programming Assignment 2/Problem 2/emgmm.m
@@ -1,11 +1,9 @@
 function Y = emgmm(x, K)
 
 [d, n] = size(x);  % the number of datax
-scale1 = min(min(x));  % the min scale of datax
-scale2 = max(max(x));  % the max scale of datax
-scale = scale1 + (scale2 - scale1);
-mu = scale * rand(d, K);
-sigma = repmat(scale*eye(d),[1 1 K]); % 初始化simag
+
+mu = [255 * rand(2, K); 512 * rand(2, K)/10];
+sigma = repmat(255*eye(d),[1 1 K]); % 初始化sigma
 pi=rand(1,K);
 pi=pi/sum(pi);
 look = pi;
@@ -19,7 +17,6 @@ for iter = 1:iteration
     % E-Step
     for i = 1:n
         for j = 1:K
-            mvnpdf(x(:, i), mu(:, j), sigma(:,:,j))
             gmm(i, j) = pi(j) * mvnpdf(x(:, i), mu(:, j), sigma(:,:,j));
         end
     end

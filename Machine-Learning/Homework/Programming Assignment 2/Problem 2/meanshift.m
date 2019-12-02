096e477f1417753e9a057e5b9245d96dea09888e
crazyang <yang3781590@163.com>
2019-12-02 17:48:32 +0800

    Update meanshift.m

--- Machine-Learning/Homework/Programming Assignment 2/Problem 2/meanshift.m
+++ Machine-Learning/Homework/Programming Assignment 2/Problem 2/meanshift.m
@@ -11,7 +11,6 @@ for i = 1:n
     look = xx(:,i);
     
     for iter = 1:iteration
-        
         k = 1 / (4 * pi^2 * h1^2 * h2^2) * exp(- 1 / (2 * h1^2) * sum((xx(1:2, i) - x(1:2, :)).^2) - 1 / (2 * h2^2) * sum((xx(3:4, i) - x(3:4, :)).^2));
         xx(:,i) = x * k' / sum(k);
         

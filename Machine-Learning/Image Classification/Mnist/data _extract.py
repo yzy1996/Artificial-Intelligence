7d7b80f30014110153eaeb5bce3ae7454c991611
crazyang <yang3781590@163.com>
2019-12-03 15:21:37 +0800

    Update data _extract.py

--- Machine-Learning/Image Classification/Mnist/data _extract.py
+++ Machine-Learning/Image Classification/Mnist/data _extract.py
@@ -10,12 +10,19 @@ from PIL import Image
 
 # struct.unpack()括号第一个参数表示按什么 fmt 来解读，第二个表示读几字节，因为整数是32位4字节，所以ii（两整数）需要8字节来读取，这两个是搭配关系，必须一致，>代表了从大到小排列
 
+
 def extract_mnist_label(filename, type):
+        
+    try:
+        os.mkdir('label')
+    except OSError:
+        pass
+    
     with open(filename, 'rb') as f:
         magic_labels, num = struct.unpack('>ii', f.read(8))   
         labels = np.fromfile(f, dtype=np.uint8)  # python的read是接着读，相当于正好现在开始读核心数据
 
-    np.savetxt('./%s_labels.txt' %type, labels, fmt='%i', delimiter=',')
+    np.savetxt('./%s/%s_labels.txt' %('label', type), labels, fmt='%i', delimiter=',')
 
 def extract_mnist_image(filename, type):
 
@@ -33,18 +40,18 @@ def extract_mnist_image(filename, type):
             image = Image.fromarray(image)  # 把数组转换为图像格式
             image.save('./%s/%s_%s.bmp' %(type, type, i), 'bmp')  # 储存到文件夹
 
-## images 存的是一个三维数组，60000*28*28，images[i]就是一个28*28的单张图片像素值 
+# images 存的是一个三维数组，60000*28*28，images[i]就是一个28*28的单张图片像素值 
 
 if __name__ == '__main__':
 
-    train_images = 'train-images.idx3-ubyte'  
-    extract_mnist_image(train_images, 'train')
+    # train_images = r'data/train-images.idx3-ubyte'  
+    # extract_mnist_image(train_images, 'train')
 
-    train_labels = 'train-labels.idx1-ubyte'
+    train_labels = r'data/train-labels.idx1-ubyte'
     extract_mnist_label(train_labels, 'train')
 
-    test_images = 't10k-images.idx3-ubyte'
-    extract_mnist_image(test_images, 'test')
+    # test_images = 'data/t10k-images.idx3-ubyte'
+    # extract_mnist_image(test_images, 'test')
 
-    test_labels = 't10k-labels.idx1-ubyte'
-    extract_mnist_label(test_labels, 'test')
+    # test_labels = 'data/t10k-labels.idx1-ubyte'
+    # extract_mnist_label(test_labels, 'test')

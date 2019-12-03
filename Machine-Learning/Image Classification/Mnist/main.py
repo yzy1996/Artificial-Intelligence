5a35594c6b4fd541024b83f19af5343af2ed9d86
crazyang <yang3781590@163.com>
2019-12-03 11:37:33 +0800

    Update main.py

--- Machine-Learning/Image Classification/Mnist/main.py
+++ Machine-Learning/Image Classification/Mnist/main.py
@@ -7,3 +7,10 @@ import tensorflow as tf
 
 # prepare work
 print("Tensorflow version " + tf.__version__)
+
+# load and prepare the mnist dataset
+# 1. use other's method 
+# 2. use my own method
+
+
+

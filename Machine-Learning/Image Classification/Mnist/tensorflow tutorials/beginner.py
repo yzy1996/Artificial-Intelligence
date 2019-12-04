48827f184e4e2275b6071d5408f40fabbd7eebb5
crazyang <yang3781590@163.com>
2019-12-04 15:36:25 +0800

    Update beginner.py

--- Machine-Learning/Image Classification/Mnist/tensorflow tutorials/beginner.py
+++ Machine-Learning/Image Classification/Mnist/tensorflow tutorials/beginner.py
@@ -3,20 +3,35 @@ import numpy as np
 import random
 
 
-mnist = tf.keras.datasets.mnist
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# 元组 x_train, x_test: uint8 数组表示的灰度图像，尺寸为 (num_samples, 28, 28)。
-# 元组 y_train, y_test: uint8 数组表示的数字标签（范围在 0-9 之间的整数），尺寸为 (num_samples,)
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# # 元组 x_train, x_test: uint8 数组表示的灰度图像，尺寸为 (num_samples, 28, 28)。
+# # 元组 y_train, y_test: uint8 数组表示的数字标签（范围在 0-9 之间的整数），尺寸为 (num_samples,)
 
-print(x_train[1])
-for i in range(len(x_train)):
-    x_train[i] = np.rot90(x_train[i], random.randint(0,4))
 
-x_test = np.genfromtxt('../data/challenge/cdigits_digits_vec.txt').reshape(150, 28, 28)
-y_test = np.genfromtxt('../data/challenge/cdigits_digits_labels.txt')
+# sequence of the image and label
+train_seq = np.genfromtxt('../data/digits4000_txt/digits4000_trainset.txt').astype(np.uint16) # (2000,2)
+test_seq = np.genfromtxt('../data/digits4000_txt/digits4000_testset.txt').astype(np.uint16) # (2000,2)
 
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# image and label
+digits_vec = np.genfromtxt('../data/digits4000_txt/digits4000_digits_vec.txt') # (4000,28,28)
+digits_vec = digits_vec.reshape(len(digits_vec), 28, 28).astype(np.uint8)
+digits_labels = np.genfromtxt('../data/digits4000_txt/digits4000_digits_labels.txt').astype(np.uint8) # (4000,)
+
+x_train = digits_vec[train_seq[:,0] - 1]
+y_train = digits_labels[train_seq[:,1] - 1]
+
+x_test = digits_vec[test_seq[:,0] - 1]
+y_test = digits_labels[test_seq[:,1] - 1]
+
+# challenge test image and label
+x_test1 = np.genfromtxt('../data/challenge/cdigits_digits_vec.txt')
+x_test1 = x_test1.reshape(len(x_test1), 28, 28).astype(np.uint8)
+y_test1 = np.genfromtxt('../data/challenge/cdigits_digits_labels.txt')
+
+
+x_train, x_test, x_test1 = x_train / 255.0, x_test / 255.0, x_test1 / 255.0
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -29,8 +44,9 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=10)
 # model.save('my_model.h5')
 model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(x_test1,  y_test1, verbose=2)
 
 # accuracy: 0.9793

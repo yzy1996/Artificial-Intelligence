173b3f05dfdde159436f14c8e12bd26aa5333a18
crazyang <yang3781590@163.com>
2020-01-04 22:04:35 +0800

    Update package_cma-es.py

--- Optimization/CMA-ES/package_cma-es.py
+++ Optimization/CMA-ES/package_cma-es.py
@@ -2,8 +2,22 @@
 '''
 
 import cma
+import numpy as np
 
-es = cma.CMAEvolutionStrategy(8 * [0], 0.5)
-es.optimize(cma.ff.rosen)
+# 初始化，均值和方差，也定义了维度
+es = cma.CMAEvolutionStrategy(2 * [0], 0.3)
 
+# 优化 Rosenbrock function（香蕉函数），能自适应维度
+# es.optimize(cma.ff.rosen)
+
+# 修改自定义目标函数
+def F(x):
+    return (x[0] - 1) ** 2 + (x[1]-2) ** 2
+
+es.optimize(F)
+
+# 打印结果
 es.result_pretty()
+
+# # 或者也可以简化为如下代码
+# xopt, es = cma.fmin2(cma.ff.rosen, 3 * [0], 0.5)
